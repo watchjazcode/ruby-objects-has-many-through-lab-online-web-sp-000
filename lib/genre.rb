11338524524a5baa67aa03dea9738ff5526c6176
Jazmin M <jazminmerckel@gmail.com>
2019-10-28 12:56:32 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -18,11 +18,15 @@ class Genre
   end
   
   def songs 
-    Song.all.select {|song| song.artist == self}
+    Song.all.select {|song| song.genre == self}
   end
   
   def artists
-    
+    collected_artists = []
+    songs.each do |song|
+      collected_artists << song.artist
+    end
+    return collected_artists
   end
 
   
